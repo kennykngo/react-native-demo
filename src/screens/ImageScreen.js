7af6651bf7c371228b8f0f68f1b49ff86d463f0a
Kenny Ngo <kenny.ngo.design@gmail.com>
2021-01-21 12:51:56 -0800

    used props to configure ImageDetail

--- src/screens/ImageScreen.js
+++ src/screens/ImageScreen.js
@@ -1,10 +1,14 @@
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
 
+import ImageDetail from "../components/ImageDetail";
+
 const ImageScreen = () => {
   return (
     <View>
-      <Text>Image Screen</Text>
+      <ImageDetail title="Forest" />
+      <ImageDetail title="Beach" />
+      <ImageDetail title="Mountain" />
     </View>
   );
 };
