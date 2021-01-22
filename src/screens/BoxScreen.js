b54be4e4ef41bd9824335ab9637b10311168fc14
Kenny Ngo <kenny.ngo.design@gmail.com>
2021-01-22 15:21:17 -0800

    created a BoxScreen to practice layout systems

---
+++ src/screens/BoxScreen.js
@@ -0,0 +1,46 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+const BoxScreen = () => {
+  return (
+    <View style={styles.parentStyle}>
+      <View style={styles.viewOneStyle} />
+      <View style={styles.viewTwoStyle} />
+      <View style={styles.viewThreeStyle} />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  parentStyle: {
+    borderWidth: 3,
+    borderColor: "black",
+    height: 200,
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+  viewOneStyle: {
+    borderWidth: 3,
+    // borderColor: "red",
+    backgroundColor: "red",
+    height: 100,
+    width: 100,
+  },
+  viewTwoStyle: {
+    borderWidth: 3,
+    // borderColor: "red",
+    backgroundColor: "green",
+    height: 100,
+    width: 100,
+    alignSelf: "flex-end",
+  },
+  viewThreeStyle: {
+    borderWidth: 3,
+    // borderColor: "red",
+    backgroundColor: "purple",
+    height: 100,
+    width: 100,
+  },
+});
+
+export default BoxScreen;
--- src/screens/TextScreen.js
+++ src/screens/TextScreen.js
@@ -1,14 +1,31 @@
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import React, { useState } from "react";
+import { StyleSheet, Text, TextInput, View } from "react-native";
 
 const TextScreen = () => {
+  const [name, setName] = useState("");
   return (
     <View>
-      <Text>Hi </Text>
+      <Text>Enter Password:</Text>
+      <TextInput
+        autoCapitalize="none"
+        autoCorrect={false}
+        style={styles.input}
+        value={name}
+        onChangeText={(newValue) => setName(newValue)}
+      />
+      {name.length < 4 ? (
+        <Text>Password has to be at least 5 characters long. </Text>
+      ) : null}
     </View>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  input: {
+    margin: 15,
+    borderColor: "black",
+    borderWidth: 1,
+  },
+});
 
 export default TextScreen;
