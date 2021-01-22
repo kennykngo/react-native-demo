aed66d78a36408d0b6f492c5eb3682d86ee0e0c4
Kenny Ngo <kenny.ngo.design@gmail.com>
2021-01-22 00:28:01 -0800

    created a TextScreen.js

--- src/screens/CounterScreen.js
+++ src/screens/CounterScreen.js
@@ -1,26 +1,42 @@
-import React, { useState } from "react";
+import React, { useReducer } from "react";
 import { Button, StyleSheet, Text, View } from "react-native";
 
+/**
+ * @function reducer
+ * @param {*} state - { count: num }
+ * @param {*} action - {type: "incremment" || "decrement", payload: 1}
+ */
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "increment":
+      return { ...state, count: state.count + action.payload };
+    case "decrement":
+      return { ...state, count: state.count - action.payload };
+    default:
+      return state;
+  }
+};
+
 const CounterScreen = () => {
-  const [counter, setCounter] = useState(0);
+  // const [counter, setCounter] = useState(0);
+  const [state, dispatch] = useReducer(reducer, { count: 0 });
 
   return (
     <View>
       <Button
         title="Increase"
         onPress={() => {
-          setCounter(counter + 1);
-          console.log(counter);
+          dispatch({ type: "increment", payload: 1 });
         }}
       />
       <Button
         title="Decrease"
         onPress={() => {
-          setCounter(counter - 1);
-          console.log(counter);
+          dispatch({ type: "decrement", payload: 1 });
         }}
       />
-      <Text>Current Count: {counter}</Text>
+      <Text>Current Count: {state.count}</Text>
     </View>
   );
 };
